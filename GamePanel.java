50bacbc25612d872b2245fff417d2a50e0a74f08
FF2DF2 <32991901+FF2DF2@users.noreply.github.com>
2017-12-05 19:58:24 -0800

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -55,19 +55,21 @@ public class GamePanel extends JPanel {
                 g2d.translate(0, r.height);
                 g2d.rotate(Math.PI);
             } // end if
-            g2d.drawImage(pipeHead, -PIPE_W/2, -PIPE_H/2, GamePanel.PIPE_W, GamePanel.PIPE_H, null);
+
             g2d.drawImage(pipeLength, -PIPE_W/2, PIPE_H/2, GamePanel.PIPE_W, r.height, null);
+            g2d.drawImage(pipeHead, -PIPE_W/2, PIPE_H/2, GamePanel.PIPE_W, GamePanel.PIPE_H, null);
             g2d.setTransform(old);
         } // end for
         g.setFont(scoreFont);
-        g.setColor(Color.white);
+        g.setColor(Color.WHITE);
         g.drawString("Score: "+fb.getScore(), 10, 20);
 
         if(fb.paused()) {
             g.setFont(pauseFont);
             g.setColor(Color.white);
-            g.drawString("PAUSED", FlappyBird.WIDTH/2-60, FlappyBird.HEIGHT/2-50);
-            g.drawString("CLICK OR PRESS SPACE TO BEGIN", FlappyBird.WIDTH/2-250, FlappyBird.HEIGHT/2+50);
+            g.drawString("PAUSED", FlappyBird.WIDTH/2-60, FlappyBird.HEIGHT/2-100);
+            g.drawString("CLICK OR PRESS SPACE TO BEGIN", FlappyBird.WIDTH/2-250, FlappyBird.HEIGHT/2);
+            g.drawString("PLEASE DON'T VOTE FOR US", FlappyBird.WIDTH/2-210, FlappyBird.HEIGHT/2+100);
         } // end if
     } // end paintComponent()
 } // end class GamePanel
